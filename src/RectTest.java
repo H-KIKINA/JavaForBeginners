040b8b62ea7138f6e33db2f8c53f3736645a5c40
H-KIKINA <zeal24360216@fsgcl.jp>
2025-05-26 10:07:08 +0900

    確認問題３

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,6 +1,17 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		fillRect(30,50,200,100);
+		setColor(255,255,0);
+		fillRect(30,40,50,100);
+		setColor(180,0,220);
+		fillRect(130,40,50,100);
+		setColor(0,255,255);
+		fillRect(30,200,150,10);
+		setColor(0,0,0);
+		fillRect(90,160,30,30);
+		setColor(100,100,100);
+		fillRect(100,120,50,50);
+		setColor(255,255,255);
+		fillRect(50,60,50,50);
 	}
 }
