c1f21dc4cf8472b1b42f05c4d88b521f153a1638
H-KIKINA <zeal24360216@fsgcl.jp>
2025-05-26 11:33:22 +0900

    練習問題3.1

---
+++ src/RectAndOval.java
@@ -0,0 +1,11 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		fillRect(110,110,110,30);
+		fillRect(130,80,70,60);
+		fillOval(120,140,30,30);
+		fillOval(180,140,30,30);
+	}
+	
+
+}
--- src/Start.java
+++ src/Start.java
@@ -1,10 +1,10 @@
 
-public class Start {
+public class Start  {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("wkwk");
-		MyFrame frame1 = new RectTest();
-	}
+		MyFrame frame1 = new RectAndOval();
 
+}
 }
